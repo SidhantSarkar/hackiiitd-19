96efef7d886704510476d4631b15e3e806fd96fc
shashwataggarwal <shashwat4699@gmail.com>
2019-07-23 15:41:01 +0530

    Fixed Countdown Time, Added Prizes Section

--- js/default-assets/active.js
+++ js/default-assets/active.js
@@ -225,8 +225,8 @@
     // :: 13.0 Countdown Active Code
     // ****************************
     if ($.fn.countdown) {
-        $("#clock").countdown("2019/08/23", function (event) {
-            $(this).html(event.strftime("<div>%m <span>Months</span></div> <div>%d <span>Days</span></div> <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div> <div>%S <span>Seconds</span></div>"));
+        $("#clock").countdown("2019/08/23 09:00:00", function (event) {
+            $(this).html(event.strftime("<div>%Y <span>Months</span></div> <div>%D <span>Days</span></div> <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div> <div>%S <span>Seconds</span></div>"));
         });
     }
 
